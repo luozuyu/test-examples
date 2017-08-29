a7e90993d6a22a104fee135b621cfc9a4be634dc
luozuyu <luozuyu@alibaba.com>
2017-08-29 16:02:31 +0800

    push

---
+++ src/test/java/App2Test.java
@@ -0,0 +1,19 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by fin_tech_2014 on 8/22/17.
+ */
+public class App2Test extends Application {
+
+    @Test
+    public void test1() {
+        Assert.assertTrue(2 > 3);
+    }
+
+    @Test
+    public void test2() {
+        Assert.assertTrue(2 < 3);
+    }
+
+}
---
+++ src/test/java/Application.java
@@ -0,0 +1,11 @@
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by fin_tech_2014 on 8/22/17.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringBootTest.class)
+public class Application {
+}
